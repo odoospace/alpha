94381aaa4f78ce44b5354a80657b26ee14ebb4c4
Javier Colmeiro <javier.colmeiro@impulzia.com>
2018-09-14 17:30:13 +0200

    + add automation of invoice template

--- addons/stock_auto_invoice/models/stock_picking.py
+++ addons/stock_auto_invoice/models/stock_picking.py
@@ -14,6 +14,15 @@ class StockPicking(models.Model):
                 res = self.sale_id.action_invoice_create(False, True)
                 invoice = self.env['account.invoice'].browse(res)
                 invoice.signal_workflow('invoice_open')
+                domain = [('name','=','Invoice - Send by Email')]
+                template = self.env['mail.template'].search(domain, limit=1)
+                template = template[0]
+                print(template.email_to)
+                template.send_mail(invoice.id, True)
                 return invoice.invoice_print()
             else:
+                domain = [('name','=','Invoice - Send by Email')]
+                template = self.env['mail.template'].search(domain, limit=1)
+                template = template[0]
+                template.send_mail(self.sale_id.invoice_ids[0].id, True)
                 return self.sale_id.invoice_ids[0].invoice_print()
